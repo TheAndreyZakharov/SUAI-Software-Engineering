d38b3481f8a351e2f0bb7a9c4c5e1869b17f0c74
Andrey <184459733+TheAndreyZakharov@users.noreply.github.com>
2024-10-12 06:27:43 +0300

    Create cars.h

---
+++ Object-Oriented Programming/15 - Описание классов и порождение объектов/cars.h
@@ -0,0 +1,21 @@
+#ifndef CARS_H
+#define CARS_H
+#include <QObject>
+#include <car.h>
+class cars : public QObject//базовым классом является класс QOBJECT
+{
+Q_OBJECT
+public:
+explicit cars(QObject *parent = nullptr);
+~cars();
+void undo();//функция манипулирует над actualdata добавляя в нее значение или null
+bool hasCars();//наличие элементов в коллекции
+car *getActualData();//функция возвращающая последний элемент коллекции
+void add(car *value);//добавление элемента в коллекцию
+QList<car *> array;//список в котором храняться элементы
+private:
+car *actualData;//последний элемент коллекции
+signals:
+void notifyObservers();//сигнал наблюдателю
+};
+#endif // CARS_H
