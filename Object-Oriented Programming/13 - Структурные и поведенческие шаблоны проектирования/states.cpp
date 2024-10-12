509161a230fab53ec50e13edbfc2d308d1e69d17
Andrey <184459733+TheAndreyZakharov@users.noreply.github.com>
2024-10-12 06:13:34 +0300

    Create states.cpp

---
+++ Object-Oriented Programming/13 - Структурные и поведенческие шаблоны проектирования/states.cpp
@@ -0,0 +1,45 @@
+//реализуем 2 задание
+#include "states.h"
+States::States(QObject *parent) : QObject(parent)
+{
+actualData = nullptr;
+States::~States()
+}
+{
+// delete: actualData
+if(actualData)
+{
+delete actualData;
+actualData = nullptr;
+}
+// delete and clear: array
+qDeleteAll(array);
+array.clear();
+void States::add(Estate *value)
+}
+{
+array.append(value);
+bool States::hasStates()
+}
+{
+return !(States::array.empty());
+Estate *States::getActualData()
+}
+{
+return actualData;
+}
+void States::undo()
+{
+if (hasStates())
+{
+array.pop_back();
+actualData = array.last();
+emit notifyObservers();
+}
+else
+actualData = nullptr;
+int States::getSize()
+}
+{
+return array.size();
+}
