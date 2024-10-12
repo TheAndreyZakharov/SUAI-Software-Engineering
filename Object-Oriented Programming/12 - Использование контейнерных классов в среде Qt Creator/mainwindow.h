bb1d44fc3e1d1e5199e119b8ef4ee9279394131f
Andrey <184459733+TheAndreyZakharov@users.noreply.github.com>
2024-10-12 06:05:58 +0300

    Create mainwindow.h

---
+++ Object-Oriented Programming/12 - Использование контейнерных классов в среде Qt Creator/mainwindow.h
@@ -0,0 +1,35 @@
+#ifndef MAINWINDOW
+H
+_
+#define MAINWINDOW_H
+#include <QMainWindow>
+#include <QFile>
+#include <QDir>
+#include <QTextStream>
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+class MainWindow : public QMainWindow
+{
+Q_OBJECT
+QList<QString> list;
+QFile fin;
+public:
+MainWindow(QWidget *parent = nullptr);
+~MainWindow();
+private slots:
+void on_pushButton_clicked();
+void on_pushButton_2_clicked();
+void on_pushButton_3_clicked();
+void on_pushButton_4_clicked();
+void on_pushButton_5_clicked();
+void on_pushButton_6_clicked();
+void on_pushButton_9_clicked();
+void on_pushButton_7_clicked();
+void on_pushButton_8_clicked();
+void on_pushButton_10_clicked();
+void on_pushButton_11_clicked();
+private:
+Ui::MainWindow *ui;
+};
+#endif // MAINWINDOW_H
