60b7f75fb59a94bc6badefa4d11f67aea1855670
Andrey <184459733+TheAndreyZakharov@users.noreply.github.com>
2024-10-12 06:20:01 +0300

    Create widget.h

---
+++ Object-Oriented Programming/14 - Порождающие шаблоны проектирования/widget.h
@@ -0,0 +1,29 @@
+#ifndef WIDGET_H
+#define WIDGET_H
+#include <QWidget>
+#include <states.h>
+#include <estate.h>
+#include <calculationfacade.h>
+namespace Ui {
+class Widget;
+}
+class Widget : public QWidget
+{
+Q_OBJECT
+public:
+explicit Widget(QWidget *parent = 0);
+~Widget();
+public slots:
+void update();
+private slots:
+void btnCalcPressed();
+void btnUndoPressed();
+private:
+Estate *processForm();
+void fillForm(Estate *value);
+void showCost(Estate *value);
+private:
+Ui::Widget *ui;
+States info;
+};
+#endif // WIDGET_H
