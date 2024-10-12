787e0bbc767a6d446bdb2d931f4d87e1bb17d86c
Andrey <184459733+TheAndreyZakharov@users.noreply.github.com>
2024-10-12 22:16:05 +0300

    Update main.lisp

--- Functional and Logic Programming/4 - Использование функционалов в языке LISP/main.lisp
+++ Functional and Logic Programming/4 - Использование функционалов в языке LISP/main.lisp
@@ -1,4 +1,54 @@
-Цель: Исследование класса задач, решаемых полным перебором и методов их решения средствами Lisp
+(defparameter *goods* nil)
+(defparameter *name* 0)
+(defparameter *quantity* 1)
+(defparameter *price* 2)
  
-Задание на лабораторную работу:
-Вариант 14. Вывести в произвольном орграфе все имеющиеся циклы без дублирования
+(defun add-good (name quantity price)
+ (push (list name quantity price) *goods*))
+ 
+(defun edit-good (name quantity price)
+ (let ((good (find name *goods* :key #'first)))
+   (when good
+     (setf (second good) quantity
+       (third good) price))))
+ 
+(defun remove-good (name)
+ (setf *goods* (remove (find name *goods* :key #'first) *goods*)))
+ 
+(defun out-of-stock ()
+ (remove-if-not (lambda (good) (= (second good) 0)) *goods*))
+ 
+(defun most-abundant ()
+ (reduce #'(lambda (a b) (if (> (second a) (second b)) a b)) *goods*))
+ 
+(defun most-expensive()
+ (reduce #'(lambda (a b) (if (> (third a) (third b)) a b)) *goods*))
+ 
+(defun find-good (name)
+ (find name *goods* :key #'first))
+ 
+(add-good 'apple 10 15)
+(add-good 'orange 20 20)
+(add-good 'banana 15 10)
+(add-good 'pear 0 10)
+ 
+(print "Все товары на складе")
+(print *goods*)(terpri)
+ 
+(edit-good 'banana 20 12)
+ 
+(print "Замена данных о банане")
+(print *goods*)(terpri)
+ 
+(print "Удаление товара")
+(print (remove-good 'orange))(terpri)
+ 
+(print "Распродано")
+(print (out-of-stock))(terpri)
+ 
+(print "Товар с минимальной ценой")
+(print (most-expensive))(terpri)
+ 
+(print "Товар с максимальной ценой")
+(print (most-abundant))(terpri)
+ 
