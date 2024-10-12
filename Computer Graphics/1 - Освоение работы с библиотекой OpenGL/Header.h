c1c0f85917fca5087bfbba01dd21f23c302327dd
Andrey <184459733+TheAndreyZakharov@users.noreply.github.com>
2024-10-12 03:55:31 +0300

    add

---
+++ Computer Graphics/1 - Освоение работы с библиотекой OpenGL/Header.h
@@ -0,0 +1,12 @@
+//
+//  Header.h
+//  cg
+//
+//  Created by Андрей Захаров on 11.02.2023.
+//
+
+#ifndef Header_h
+#define Header_h
+
+
+#endif /* Header_h */
---
+++ Computer Graphics/1 - Освоение работы с библиотекой OpenGL/models.h
@@ -0,0 +1,47 @@
+/*
+ Здесь храянтся модели
+ */
+
+#include <iostream>
+#include <GL/freeglut.h>
+
+void model_cube() {
+    glBegin(GL_QUADS);
+    
+    glNormal3f(0.0, 1.0, 0.0);//нормали
+    glTexCoord2f(0.0f, 0.0f);//икс коорд glVertex3f( 1.0f, 1.0f, -1.0f);//вершины
+    glTexCoord2f(0.0f, 1.0f);//игрик коорд glVertex3f(-1.0f, 1.0f, -1.0f);
+    glTexCoord2f(1.0f, 1.0f); glVertex3f(-1.0f, 1.0f,  1.0f);
+    glTexCoord2f(1.0f, 0.0f); glVertex3f( 1.0f, 1.0f,  1.0f);
+    
+    glNormal3f(0.0, -1.0, 0.0);
+    glTexCoord2f(0.0f, 0.0f); glVertex3f( 1.0f, -1.0f,  1.0f);
+    glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0f, -1.0f,  1.0f);
+    glTexCoord2f(1.0f, 1.0f); glVertex3f(-1.0f, -1.0f, -1.0f);
+    glTexCoord2f(1.0f, 0.0f); glVertex3f( 1.0f, -1.0f, -1.0f);
+    
+    glNormal3f(0.0, 0.0, 1.0);
+    glTexCoord2f(0.0f, 0.0f); glVertex3f( 1.0f,  1.0f, 1.0f);
+    glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0f,  1.0f, 1.0f);
+    glTexCoord2f(1.0f, 1.0f); glVertex3f(-1.0f, -1.0f, 1.0f);
+    glTexCoord2f(1.0f, 0.0f); glVertex3f( 1.0f, -1.0f, 1.0f);
+    
+    glNormal3f(0.0, 0.0, -1.0);
+    glTexCoord2f(0.0f, 0.0f); glVertex3f( 1.0f, -1.0f, -1.0f);
+    glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0f, -1.0f, -1.0f);
+    glTexCoord2f(1.0f, 1.0f); glVertex3f(-1.0f,  1.0f, -1.0f);
+    glTexCoord2f(1.0f, 0.0f); glVertex3f( 1.0f,  1.0f, -1.0f);
+    
+    glNormal3f(-1.0, 0.0, 0.0);
+    glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.0f,  1.0f,  1.0f);
+    glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0f,  1.0f, -1.0f);
+    glTexCoord2f(1.0f, 1.0f); glVertex3f(-1.0f, -1.0f, -1.0f);
+    glTexCoord2f(1.0f, 0.0f); glVertex3f(-1.0f, -1.0f,  1.0f);
+    
+    glNormal3f(1.0f, 0.0f, 0.0f);
+    glTexCoord2f(0.0f, 0.0f); glVertex3f(1.0f,  1.0f, -1.0f);
+    glTexCoord2f(0.0f, 1.0f); glVertex3f(1.0f,  1.0f,  1.0f);
+    glTexCoord2f(1.0f, 1.0f); glVertex3f(1.0f, -1.0f,  1.0f);
+    glTexCoord2f(1.0f, 0.0f); glVertex3f(1.0f, -1.0f, -1.0f);
+    glEnd();
+}
