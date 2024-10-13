d24c24d0238b7a6f5b3221cd2f45faaea7c537b0
Andrey <184459733+TheAndreyZakharov@users.noreply.github.com>
2024-10-14 01:32:39 +0300

    add

---
+++ JavaScript, its Libraries and Frameworks in Frontend Development/Практико-Ориентированный Проект. Vue.js, Single Page Application (SPA)/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <AppNavbar/>
+    <HomePage/>
+    <SmartPlugInfo/>
+    <ImageSlider/>
+    <FeedbackForm/>
+    <AppFooter/>
+  </div>
+</template>
+
+<script>
+import AppNavbar from './components/AppNavbar.vue'
+import HomePage from './components/HomePage.vue'
+import SmartPlugInfo from './components/SmartPlugInfo.vue'
+import ImageSlider from './components/ImageSlider.vue'
+import FeedbackForm from './components/FeedbackForm.vue'
+import AppFooter from './components/AppFooter.vue'
+
+
+
+export default {
+  name: 'App',
+  components: {
+    AppNavbar,
+    HomePage,
+    SmartPlugInfo,
+    ImageSlider,
+    FeedbackForm,
+    AppFooter
+
+  }
+}
+</script>
+
+<style>
+/* Импорт глобальных стилей */
+@import './assets/main.css';
+</style>
