450e5ea2577902e225a6a629559843c603ac8adc
Andrey <184459733+TheAndreyZakharov@users.noreply.github.com>
2024-10-13 21:26:52 +0300

    Create GameApp.swift

---
+++ Mobile Device Programming/7 - Использование геолокационных сервисов/GameApp.swift
@@ -0,0 +1,18 @@
+//
+//  GameApp.swift
+//  Game
+//
+//  Created by Андрей Захаров on 08.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
