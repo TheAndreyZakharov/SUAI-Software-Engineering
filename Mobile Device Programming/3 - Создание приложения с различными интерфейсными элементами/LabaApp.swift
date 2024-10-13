9663398995c9063d04103c7f6654a5a61e158191
Andrey <184459733+TheAndreyZakharov@users.noreply.github.com>
2024-10-13 21:19:14 +0300

    Create LabaApp.swift

---
+++ Mobile Device Programming/3 - Создание приложения с различными интерфейсными элементами/LabaApp.swift
@@ -0,0 +1,17 @@
+//
+//  LabaApp.swift
+//  Laba
+//
+//  Created by Андрей Захаров on 28.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct LabaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
