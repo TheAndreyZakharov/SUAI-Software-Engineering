82932b35a75603828713ce180b2a2ec0a59a41d2
Andrey <184459733+TheAndreyZakharov@users.noreply.github.com>
2024-10-13 10:53:21 +0300

    Create myKafkaProducer.java

---
+++ Technology of Server Information Systems Development/7 - Разработка приложения с асинхронной очередью сообщений/myKafkaProducer.java
@@ -0,0 +1,24 @@
+package com.example.Project;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyKafkaProducer {
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    @Autowired
+    public MyKafkaProducer(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendMessage(String topic, String key, String message) {
+        kafkaTemplate.send(topic, key, message);
+    }
+}
+
+
+
+
+
