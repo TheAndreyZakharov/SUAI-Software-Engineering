d033e5f8a7dddea38aebb63d153a491ba20d779c
Andrey <184459733+TheAndreyZakharov@users.noreply.github.com>
2024-10-13 10:46:49 +0300

    Create Itemobjrepository.java

---
+++ Technology of Server Information Systems Development/6 - Разработка AJAX приложения с использованием Hibernate/Itemobjrepository.java
@@ -0,0 +1,6 @@
+package com.example.ThymeleafProject;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemObjRepository extends JpaRepository<ItemObj, Integer> {
+}
