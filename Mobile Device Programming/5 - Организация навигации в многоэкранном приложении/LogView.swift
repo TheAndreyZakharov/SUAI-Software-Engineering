fc40645a8b9c9a81da23e3161bfd79595291e653
Andrey <184459733+TheAndreyZakharov@users.noreply.github.com>
2024-10-13 21:23:30 +0300

    Create LogView.swift

---
+++ Mobile Device Programming/5 - Организация навигации в многоэкранном приложении/LogView.swift
@@ -0,0 +1,44 @@
+//
+//  LogView.swift
+//  Laba
+//
+//  Created by Андрей Захаров on 07.03.2024.
+//
+
+import SwiftUI
+
+struct LogView: View {
+    var logs: [String] // Массив строк с записями лога
+    
+    var body: some View {
+        VStack {
+            Text("📒")
+                .font(.largeTitle)
+                .padding()
+            Text("Лог операций")
+                .font(.title)
+                .padding()
+            
+            // Проверяем, есть ли записи в логе
+            if logs.isEmpty {
+                Text("Записей в логе нет")
+                    .padding()
+            } else {
+                List(logs.reversed(), id: \.self) { log in
+                    Text(log)
+                }
+            }
+        }
+    }
+}
+
+// Предпросмотр
+struct LogView_Previews: PreviewProvider {
+    static var previews: some View {
+        // Создание примера данных для предпросмотра
+        let sampleLogs = ["Покупка: 🥤 за 50🪙", "Выигрыш: 500 в режиме 1 - 10"]
+        
+        // Возвращаем LogView с примерными данными
+        LogView(logs: sampleLogs)
+    }
+}
